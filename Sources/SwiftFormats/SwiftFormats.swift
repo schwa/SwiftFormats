746caf7935d17e427056b4eb831acf0e608cfa1a
Jonathan Wight <jwight@mac.com>
2023-01-30 10:38:57 -0800

    Initial swift package.

---
+++ Sources/SwiftFormats/SwiftFormats.swift
@@ -0,0 +1,6 @@
+public struct SwiftFormats {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftFormatsTests/SwiftFormatsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftFormats
+
+final class SwiftFormatsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftFormats().text, "Hello, World!")
+    }
+}
