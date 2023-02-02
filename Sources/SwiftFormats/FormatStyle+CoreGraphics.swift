1ee477dd2462b43c1faa0bf4afbf6b99912992d1
brett ohland <brett@ohland.ca>
2023-02-02 08:30:13 -0700

    CGPoint FormatStyle Updates
    - Adds convenience initializer for parsing CGPoint values
    - Adds supporting tests

--- Sources/SwiftFormats/FormatStyle+CoreGraphics.swift
+++ Sources/SwiftFormats/FormatStyle+CoreGraphics.swift
@@ -45,6 +45,32 @@ public struct CGPointParseStrategy: ParseStrategy {
 // MARK: -
 
 public extension CGPoint {
+    init(_ string: String) throws {
+        self = try CGPointFormatStyle().parseStrategy.parse(string)
+    }
+
+    init<T, Value>(_ value: Value, format: T) throws where T: ParseableFormatStyle, Value: StringProtocol, T.Strategy == CGPointParseStrategy {
+        self = try format.parseStrategy.parse(value.description)
+    }
+
+    init<T, Value>(_ value: Value, strategy: T) throws where T: ParseStrategy, Value: StringProtocol, T.ParseInput == String, T.ParseOutput == CGPoint {
+        self = try strategy.parse(value.description)
+    }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension ParseableFormatStyle where Self == CGPointFormatStyle {
+    static var cgPoint: Self { .init() }
+}
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+public extension ParseStrategy where Self == CGPointParseStrategy {
+    static var cgPoint: Self { .init() }
+}
+
+// MARK: -
+
+public extension CGPoint {
     func formatted<S>(_ format: S) -> S.FormatOutput where Self == S.FormatInput, S : FormatStyle {
         return format.format(self)
     }
