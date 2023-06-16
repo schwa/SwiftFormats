6ea63e3af409187ca73fa1d4e96b73d092888835
Jonathan Wight <jwight@mac.com>
2023-06-16 16:46:17 -0700

    CGSize formatter.

--- Sources/SwiftFormats/FormatStyle+CoreGraphics.swift
+++ Sources/SwiftFormats/FormatStyle+CoreGraphics.swift
@@ -81,3 +81,87 @@ public extension CGPoint {
         formatted(CGPointFormatStyle())
     }
 }
+
+// MARK: CGSize
+
+/// Format `CGSize`s.
+public struct CGSizeFormatStyle: ParseableFormatStyle {
+
+    public var parseStrategy: CGSizeParseStrategy {
+        return CGSizeParseStrategy(componentFormat: componentFormat)
+    }
+
+    public var componentFormat: FloatingPointFormatStyle<Double> = .number
+
+    public init() {
+    }
+
+    public func format(_ value: CGSize) -> String {
+        let style = SimpleListFormatStyle(substyle: componentFormat)
+        return style.format([value.width, value.height])
+    }
+}
+
+public extension FormatStyle where Self == CGSizeFormatStyle {
+    static var size: Self {
+        return Self()
+    }
+}
+
+// MARK: -
+
+public struct CGSizeParseStrategy: ParseStrategy {
+    public var componentFormat: FloatingPointFormatStyle<Double>
+
+    public init(componentFormat: FloatingPointFormatStyle<Double> = .number) {
+        self.componentFormat = componentFormat
+    }
+
+    public func parse(_ value: String) throws -> CGSize {
+        var strategy = SimpleListFormatStyle(substyle: componentFormat).parseStrategy
+        strategy.countRange = 2 ... 2
+        let scalars = try strategy.parse(value)
+        return CGSize(width: scalars[0], height: scalars[1])
+    }
+}
+
+// MARK: -
+
+public extension CGSize {
+    init(_ string: String) throws {
+        self = try CGSizeFormatStyle().parseStrategy.parse(string)
+    }
+
+    init<Format, ParseInput>(_ input: ParseInput, format: Format) throws where Format: ParseableFormatStyle, ParseInput: StringProtocol, Format.Strategy == CGSizeParseStrategy {
+        self = try format.parseStrategy.parse(String(input))
+    }
+
+    init<Strategy, Value>(_ value: Value, strategy: Strategy) throws where Strategy: ParseStrategy, Value: StringProtocol, Strategy.ParseInput == String, Strategy.ParseOutput == CGSize {
+        self = try strategy.parse(String(value))
+    }
+}
+
+public extension ParseableFormatStyle where Self == CGSizeFormatStyle {
+    static var point: Self {
+        .init()
+    }
+}
+
+public extension ParseStrategy where Self == CGSizeParseStrategy {
+    static var point: Self {
+        .init()
+    }
+}
+
+// MARK: -
+
+public extension CGSize {
+    func formatted<S>(_ format: S) -> S.FormatOutput where Self == S.FormatInput, S: FormatStyle {
+        return format.format(self)
+    }
+
+    func formatted() -> String {
+        formatted(CGSizeFormatStyle())
+    }
+}
+
